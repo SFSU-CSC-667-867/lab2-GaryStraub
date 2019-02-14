c99b205c6023238f7a1abf6659767604b5c6bb00
GaryStraub <gstraub@sfsu.edu>
2019-02-13 20:21:19 -0800

    Stuff

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+// console.log('Hello World');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log(req.query);
+    res.send('Hello World!');
+});
+app.get('/hello', (req, res) => res.send('World!'));
+app.get('/add', (req, res) => res.send('0'));
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
